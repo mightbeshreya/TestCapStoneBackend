57d5bf7e8924cdeccc68105f738b5f70a87f46cf
shreya <shreya.sdeshpande06@gmail.com>
2020-11-02 17:55:51 +0530

    Pushing Comments into Customer Controller and Category Controller

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CategoryController.java
@@ -18,40 +18,56 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
+/*This is a Rest Controller Which handles two endpoints :
+1. Get All Categories - “/category” : Getting All the categories from the database.
+It is a GET request and will not require any parameters from the user.
+2.Get Category by Id - “/category/{category_id}”
+It is a GET Request and takes Category UUID in category_id parameter as path variable
+ */
 @RestController
 @RequestMapping("")
 @CrossOrigin
 public class CategoryController {
 
+    /*Performing Services through Category Service */
     @Autowired
     private CategoryService categoryService;
 
+    /* GET ALL Categories present in Database */
     @RequestMapping(method = RequestMethod.GET, path = "/category", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<CategoriesListResponse> getAllCategories() {
+        /* All categories are ordered by Name and called through service */
         List<CategoryEntity> categoryEntities = categoryService.getAllCategoriesOrderedByName();
 
+        /* Categories List Not empty from Database */
         if (!categoryEntities.isEmpty()) {
+            //Adding into Category List Responses each Category List Response
             List<CategoryListResponse> categoryListResponses = new LinkedList<>();
+            //Mapping Category Entity to Category List Response
             categoryEntities.forEach(categoryEntity -> {
                 CategoryListResponse categoryListResponse = new CategoryListResponse()
                         .id(UUID.fromString(categoryEntity.getUuid()))
                         .categoryName(categoryEntity.getCategoryName());
                 categoryListResponses.add(categoryListResponse);
             });
+            //Returning Category List Response
             CategoriesListResponse categoriesListResponse = new CategoriesListResponse().categories(categoryListResponses);
             return new ResponseEntity<CategoriesListResponse>(categoriesListResponse, HttpStatus.OK);
         } else {
+            //If Category List In Database is Empty, returning Empty CategoriesListResponse
             return new ResponseEntity<CategoriesListResponse>(new CategoriesListResponse(), HttpStatus.OK);
         }
     }
 
+    /* Get Category by Category UUID by taking it as Path Variable */
     @RequestMapping(method = RequestMethod.GET, path = "/category/{category_id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<CategoryDetailsResponse> getCategoryById(@PathVariable(value = "category_id") final String categoryUuid) throws CategoryNotFoundException {
 
+        /* Getting Category Entity By Category UUID */
         CategoryEntity categoryEntity = categoryService.getCategoryById(categoryUuid);
-
+        /* Get Items From Category Foiund */
         List<ItemEntity> itemEntities = categoryEntity.getItems();
-
+        /* Add Item Entities from the category to List of Item List  Model*/
         List<ItemList> itemLists = new LinkedList<>();
         itemEntities.forEach(itemEntity -> {
             ItemList itemList = new ItemList()
@@ -62,6 +78,7 @@ public class CategoryController {
             itemLists.add(itemList);
         });
 
+        /* Sending Category Details into categoryDetailsResponse model and returning the response */
         CategoryDetailsResponse categoryDetailsResponse = new CategoryDetailsResponse()
                 .categoryName(categoryEntity.getCategoryName())
                 .id(UUID.fromString(categoryEntity.getUuid()))
--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -22,23 +22,16 @@ import java.util.UUID;
 @RequestMapping("")
 @CrossOrigin
 public class CustomerController {
+    /*  AutoWiring customer service */
     @Autowired
     CustomerService customerService;
-
-    @Autowired
-    LogoutBusinessService logoutBusinessService;
-
-    @Autowired
-    UpdatedCustomerBusinessService updatedCustomerBusinessService;
-
-    @Autowired
-    UpdatePasswordBusinessService updatePasswordBusinessService;
 
     /* The method handles Customer SignUp Related request.It takes the details as per in the SignupCustomerRequest
     & produces response in SignupCustomerResponse and returns UUID of newly Created Customer and Success message else Return error code and error Message.
      */
     @RequestMapping(method = RequestMethod.POST, path = "/customer/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupCustomerResponse> signup(@RequestBody(required = false) final SignupCustomerRequest signupCustomerRequest) throws SignUpRestrictedException {
+        /* If any field other than Last name is Null, throwing Exception SGR-005 */
         if (signupCustomerRequest.getFirstName() == null
                 || signupCustomerRequest.getEmailAddress() == null
                 || signupCustomerRequest.getPassword() == null
@@ -49,6 +42,9 @@ public class CustomerController {
                 || signupCustomerRequest.getContactNumber().isEmpty()) {
             throw new SignUpRestrictedException("SGR-005", "Except last name all fields should be filled");
         }
+        /* Creating New Customer Entity and saving in Database and sending created Customer UUID as
+        response and returning response
+         */
         CustomerEntity customerEntity = new CustomerEntity();
         customerEntity.setUuid(UUID.randomUUID().toString());
         customerEntity.setEmail(signupCustomerRequest.getEmailAddress());
@@ -67,14 +63,15 @@ public class CustomerController {
      */
     @RequestMapping(method = RequestMethod.POST, path = "/customer/login", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<LoginResponse> login(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException {
-        /*if(!authorization.matches("^.+[\\:]{1,1}.+$")){
-            throw new AuthenticationFailedException("ATH-003", "Incorrect format of decoded customer name and password");
-        }*/
+        /* Decoding the authorization token to get Access Token and UserName and Password */
         authFormatCheck(authorization);
         byte[] decode = Base64.getDecoder().decode(authorization.split(" ")[1]);
         String decodedText = new String(decode);
         String[] decodedArray = decodedText.split(":");
 
+        /* Getting Customer from Customer Auth Token if Customer Exists, setting header with access token and
+        * sending login response
+        *  */
         CustomerAuthEntity customerAuthTokenEntity = customerService.authenticate(decodedArray[0], decodedArray[1]);
         CustomerEntity customerEntity = customerAuthTokenEntity.getCustomer();
         LoginResponse loginResponse = new LoginResponse().id(customerEntity.getUuid()).message("LOGGED IN SUCCESSFULLY");
@@ -89,37 +86,40 @@ public class CustomerController {
     @RequestMapping(method = RequestMethod.POST, path = "/customer/logout", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<LogoutResponse> logout(@RequestHeader("authorization") final String authorization)
             throws AuthorizationFailedException {
+        /* Decoding Authorization to get access token and username and password */
         String[] authorizationData = authorization.split(" ");
         String accessToken = authorizationData[1];
+        /* Sending Access Token For Logging Out  */
         CustomerAuthEntity customerAuthEntity = new CustomerAuthEntity();
         customerAuthEntity = customerService.logout(accessToken);
         CustomerEntity customer = customerAuthEntity.getCustomer();
+        /* Sending Logout Response */
         LogoutResponse authorizedLogoutResponse = new LogoutResponse().id(customer.getUuid()).message("LOGGED OUT SUCCESSFULLY");
         return new ResponseEntity<LogoutResponse>(authorizedLogoutResponse,  HttpStatus.OK);
     }
-    /*@RequestMapping(method = RequestMethod.POST, path = "/customer/logout", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<LogoutResponse> logOut(@RequestHeader("authorization") final String authorization) throws AuthenticationFailedException, AuthorizationFailedException {
-        CustomerEntity logOutUser = customerService.logout(authorization);
-        LogoutResponse logoutResponse = new LogoutResponse().id(logOutUser.getUuid()).message("LOGGED OUT SUCCESSFULLY");
-        return new ResponseEntity<LogoutResponse>(logoutResponse, HttpStatus.OK);
-    } */
 
     /* This method handles the customer details update request. Takes the request as UpdateCustomerRequest and produces UpdateCustomerResponse containing the details of the updated Customer.
     If error returns the error code with corresponding Message.
      */
     @RequestMapping(method = RequestMethod.PUT, path = "/customer", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<UpdateCustomerResponse> updateCustomer(@RequestHeader("authorization") final String authorization, @RequestBody(required = false) UpdateCustomerRequest updateCustomerRequest) throws AuthorizationFailedException, UpdateCustomerException {
+    public ResponseEntity<UpdateCustomerResponse> updateCustomer(@RequestHeader("authorization") final String authorization,
+                        @RequestBody(required = false) UpdateCustomerRequest updateCustomerRequest)
+            throws AuthorizationFailedException, UpdateCustomerException {
+        /* FirstName Field Should not be empty in  UpdateCustomerRequest*/
         if(updateCustomerRequest.getFirstName().isEmpty()) {
             throw new UpdateCustomerException("UCR-002", "First name field should not be empty");
         }
+        /* Decoding Authorization to get access token and username and password */
         String[] authorizationData = authorization.split("Bearer ");
         String userAccessToken = authorizationData[1];
+        /* Sending Customer Entity For Updating Customer Entity Details in Database */
         CustomerEntity customerEntity = customerService.getCustomer(userAccessToken);
 
         customerEntity.setFirstName(updateCustomerRequest.getFirstName());
         customerEntity.setLastName(updateCustomerRequest.getLastName());
 
         CustomerEntity updateCustomer = customerService.updateCustomer(customerEntity);
+        /* Sending UpdateCustomerResponse */
         UpdateCustomerResponse updateCustomerResponse = new UpdateCustomerResponse().id(updateCustomer.getUuid()).firstName(updateCustomer.getFirstname()).lastName(updateCustomer.getLastname())
                 .status("CUSTOMER DETAILS UPDATED SUCCESSFULLY");
         return new ResponseEntity<UpdateCustomerResponse>(updateCustomerResponse, HttpStatus.OK);
@@ -129,23 +129,29 @@ public class CustomerController {
      */
 
     @RequestMapping(method = RequestMethod.PUT, path = "/customer/password", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<UpdatePasswordResponse> updatePassword(@RequestHeader("authorization") final String authorization, @RequestBody(required = false) UpdatePasswordRequest updatePasswordRequest ) throws AuthorizationFailedException, UpdateCustomerException{
+    public ResponseEntity<UpdatePasswordResponse> updatePassword(@RequestHeader("authorization") final String authorization,
+                                                                 @RequestBody(required = false) UpdatePasswordRequest updatePasswordRequest )
+            throws AuthorizationFailedException, UpdateCustomerException{
+        /* If Old or new password is empty, throw error */
         if(updatePasswordRequest.getNewPassword().isEmpty() || updatePasswordRequest.getOldPassword().isEmpty()) {
             throw new UpdateCustomerException("UCR-003", "No field should be empty");
         }
-
+        /* Decoding Authorization to get access token and USername and Password */
         String[] authorizationData = authorization.split("Bearer ");
         String userAccessToken = authorizationData[1];
+        /* Sending Customer Entity To Update Password */
         CustomerEntity customerEntity = customerService.getCustomer(userAccessToken);
 
         String newPassword = updatePasswordRequest.getNewPassword();
         String oldPassword = updatePasswordRequest.getOldPassword();
         CustomerEntity updatedCustomerEntity = customerService.updateCustomerPassword(oldPassword, newPassword, customerEntity);
+        /* Sending UpdatePasswordResponse */
         UpdatePasswordResponse updatePasswordResponse = new UpdatePasswordResponse().id(updatedCustomerEntity.getUuid())
                 .status("CUSTOMER PASSWORD UPDATED SUCCESSFULLY");
         return new ResponseEntity<UpdatePasswordResponse>(updatePasswordResponse, HttpStatus.OK);
     }
 
+    /* This Method Performs check whether Authorization is in correct format or not - Called In Login*/
     public void authFormatCheck (final String authorization) throws AuthenticationFailedException{
         try {
             byte[] decoded = Base64.getDecoder().decode(authorization.split(" ")[1]);
