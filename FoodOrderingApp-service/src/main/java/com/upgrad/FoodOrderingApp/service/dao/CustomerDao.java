b28bdb7ac95b910c4faed258dc224e90d7c1f5cc
Mamta190 <64964289+Mamta190@users.noreply.github.com>
2020-11-02 19:42:37 +0530

    Update CustomerDao.java

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerDao.java
@@ -8,18 +8,22 @@ import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
+//This Class is created to access DB with respect to CustomerAddress entity
+
 @Repository
 public class CustomerDao {
 
     @PersistenceContext
     private EntityManager entityManager;
 
-
+   //To Create Customer.
     public CustomerEntity createCustomer(CustomerEntity customerEntity) {
         entityManager.persist(customerEntity);
         return customerEntity;
     }
 
+    
+   //To check contact no. is exist
     public CustomerEntity IsContactNumberExists(final String contactNumber) {
         try {
             return entityManager.createNamedQuery("customerByContactNumber", CustomerEntity.class).setParameter("contactNumber", contactNumber).getSingleResult();
@@ -28,7 +32,8 @@ public class CustomerDao {
             return null;
         }
     }
-
+    
+   //To check Uuid.
     public CustomerEntity checkUUID(final String uuid) {
         try {
             return entityManager.createNamedQuery("userByUuid", CustomerEntity.class).setParameter("uuid", uuid)
@@ -38,6 +43,7 @@ public class CustomerDao {
         }
     }
 
+    //To get customer by email.
     public CustomerEntity getCustomerByEmail(String email) {
         try {
             return entityManager.createNamedQuery("userByEmail", CustomerEntity.class).setParameter("email", email)
@@ -47,11 +53,13 @@ public class CustomerDao {
         }
     }
 
+    //To create Authority token
     public CustomerAuthEntity createAuthToken(CustomerAuthEntity customerAuthTokenEntity) {
         entityManager.persist(customerAuthTokenEntity);
         return customerAuthTokenEntity;
     }
 
+    //To check auth token
     public CustomerAuthEntity checkAuthToken(String accessToken) {
         try {
             return entityManager.createNamedQuery("getToken", CustomerAuthEntity.class).setParameter("accessToken", accessToken).getSingleResult();
@@ -60,20 +68,24 @@ public class CustomerDao {
         }
     }
 
+    //To update customer auth token
     public void updateCustomerAuthToken(CustomerAuthEntity customerAuthTokenEntity) {
         entityManager.merge(customerAuthTokenEntity);
     }
 
+    //To update customer datils
     public CustomerEntity updateCustomerDetails(CustomerEntity customerEntity) {
         entityManager.merge(customerEntity);
         return customerEntity;
     }
 
+    //To update password
     public CustomerEntity updatePassword(CustomerEntity updatedCustomerPassword) {
         entityManager.merge(updatedCustomerPassword);
         return updatedCustomerPassword;
     }
 
+    //To get customer by Uuid
     public CustomerEntity getCustomerByUuid(String customerUuid) {
         try {
             CustomerEntity customer = entityManager.createNamedQuery("userByUuid", CustomerEntity.class).setParameter("uuid", customerUuid).getSingleResult();
@@ -83,6 +95,7 @@ public class CustomerDao {
         }
     }
 
+    // To get user auth Token
     public CustomerAuthEntity getUserAuthToken(final String accessToken) {
         try {
             return entityManager.createNamedQuery("customerAuthTokenByAccessToken", CustomerAuthEntity.class).setParameter("accessToken", accessToken).getSingleResult();
@@ -91,6 +104,7 @@ public class CustomerDao {
         }
     }
 
+    //To update customer log out
     public void updateCustomerLogoutAt(final CustomerAuthEntity customerAuthToken) {
         entityManager.merge(customerAuthToken);
     }
