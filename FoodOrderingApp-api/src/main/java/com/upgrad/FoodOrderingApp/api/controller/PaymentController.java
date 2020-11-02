3b65318fae1e64939719e79888de0c729028d9c9
Mamta190 <64964289+Mamta190@users.noreply.github.com>
2020-11-02 19:10:22 +0530

    Update PaymentController.java

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/PaymentController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/PaymentController.java
@@ -16,17 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.UUID;
 
+// Payment Controller Handles all  the Payment related endpoints
+
 @RestController
 @RequestMapping("")
 @CrossOrigin
 public class PaymentController {
     @Autowired
-    private PaymentService paymentService;
+    private PaymentService paymentService; // Handles all the Service Related to Payment.
 
+    /* The method handles get all payment methods fro the order.
+    & produces response in PaymentListResponse and returns details of all the payment methods UUID and Name and if error returns error code and error Message.
+    */
     @RequestMapping(method = RequestMethod.GET, path = "/payment", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<PaymentListResponse> getPaymentMethods() {
+        
+        
+        //Calls getAllPaymentMethods of paymentService tp get all the paymentEntity in DB
         List<PaymentEntity> listOfPaymentEntities = paymentService.getAllPaymentMethods();
 
+        
+        //Creating PaymentListResponse by adding list of PaymentResponse to it
         PaymentListResponse paymentListResponse = new PaymentListResponse();
 
         for(PaymentEntity paymentEntity: listOfPaymentEntities) {
